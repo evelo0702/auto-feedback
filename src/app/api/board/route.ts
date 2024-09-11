44afb66623047d0748fd2ac296059c00763d40f5
EVELO <evelo0702@gmail.com>
2024-09-11 19:14:33 +0900

    feat : 몽고db 연결 / 데이터 테스트

---
+++ src/app/api/board/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/app/utils/database";
+
+// GET 요청 처리
+export async function GET() {
+  try {
+    const db = (await connectDB).db("auto-feedback");
+    const data = await db.collection("faqs").find().toArray();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
