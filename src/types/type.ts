b714be7641cb8a6e5e0026b44bca31e64c87781b
EVELO <evelo0702@gmail.com>
2024-09-12 17:00:26 +0900

    update : 타입에 카테고리 대신 카테고리 코드 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,10 +1,11 @@
 export interface Faqs {
   id: number;
-  category: string;
+  category_code: number;
   content: { question: string; answer: string };
 }
 export interface Notices {
   id: number;
   title: string;
   content: string;
+  category_code: number;
 }
