7a19733af483ec8bbcca7c735d1cf7933214cb55
EVELO <evelo0702@gmail.com>
2024-09-12 16:59:55 +0900

    feat: 카테고리 분류를 위한 파일

---
+++ src/app/utils/categoryCode.ts
@@ -0,0 +1,42 @@
+export const categoryCode = [
+  {
+    id: 1,
+    kor: "계정 관리",
+    eng: "AccountManagement",
+  },
+  {
+    id: 2,
+    kor: "게임 플레이",
+    eng: "Gameplay",
+  },
+  {
+    id: 3,
+    kor: "기술 지원",
+    eng: "TechnicalSupport",
+  },
+  {
+    id: 4,
+    kor: "결제 및 아이템",
+    eng: "PaymentAndItems",
+  },
+  {
+    id: 5,
+    kor: "커뮤니티 및 소셜",
+    eng: "CommunityAndSocial",
+  },
+  {
+    id: 6,
+    kor: "업데이트 및 패치",
+    eng: "UpdatesAndPatches",
+  },
+  {
+    id: 7,
+    kor: "보안",
+    eng: "Security",
+  },
+  {
+    id: 8,
+    kor: "게임 규칙 및 정책",
+    eng: "GameRulesAndPolicies",
+  },
+];
