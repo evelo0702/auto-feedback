90cbf175cdae7cecebfffb97f561a0139ad78048
EVELO <evelo0702@gmail.com>
2024-09-13 20:27:26 +0900

    update: 클라이언트 컴포넌트를 사용하기위해 변경된 faqs페이지

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,32 +1,16 @@
-import FaqListCard from "@/components/faq-list-card";
+
 import { getFaqData } from "@/lib/getFaqData";
 import SortCategory from "../utils/sortCategory";
 import { categoryCode } from "../utils/categoryCode";
-import { Faqs } from "@/types/type";
+import FaqList from "@/components/faq-list";
 
-const FaqListPage = async () => {
-  const data = await getFaqData();
+export default async function Page() {
+  const data = await getFaqData("");
   const sortData = SortCategory(data);
 
   return (
     <div className="flex justify-center">
-      <div>
-        {categoryCode.map((i) => (
-          <div key={i.id}>
-            <p className="text-center text-xl mb-2">
-              {i.kor} ({i.eng})
-            </p>
-            {sortData[i.eng].map((item: Faqs) => (
-              <div key={item.id} className="border-2 rounded-lg mb-4 p-2">
-                <div>Q: {item.content.question}</div>
-                <div>A: {item.content.answer}</div>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+      <FaqList categoryCode={categoryCode} sortData={sortData} />
     </div>
   );
-};
-
-export default FaqListPage;
+}
