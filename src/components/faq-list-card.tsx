41744500ed49c5eb40a09868114742cb9f0028bb
EVELO <evelo0702@gmail.com>
2024-09-13 20:26:25 +0900

    update: 디자인변경

--- src/components/faq-list-card.tsx
+++ src/components/faq-list-card.tsx
@@ -7,9 +7,9 @@ const FaqListCard = (item: Faqs) => {
   return (
     <>
       <button onClick={() => setShowAns(!ShowAns)}>
-        <div className="bg-gray-100 border-2 mb-1 p-2 border-neutral-300 rounded-md flex-1 flex items-center">
+        <div className="hover:bg-gray-100 border-2 mb-1 p-2 border-neutral-300 rounded-md flex-1 flex items-center">
           <div className="ms-4">{ShowAns ? "-" : "+"}</div>
-          <div className="w-1/3 me-2">[{item.category}]</div>
+          <div className="w-1/3 me-2">[{item.category_name.kor}]</div>
           <div>{item.content.question}</div>
         </div>
       </button>
--- src/components/notice-list.tsx
+++ src/components/notice-list.tsx
@@ -5,7 +5,7 @@ const NoticeList = (item: Notices) => {
   return (
     <div>
       <Link href={`/notice/${item.id}`}>
-        <div className="bg-gray-100 border-2 mb-1 p-2 border-neutral-300 rounded-md flex-1 flex items-center">
+        <div className="hover:bg-gray-100 border-2 mb-1 p-2 border-neutral-300 rounded-md flex-1 flex items-center">
           <div className="ms-4">{item.title}</div>
         </div>
       </Link>
