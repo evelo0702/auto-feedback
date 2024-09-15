69b6de6b4977405836a645072675f1991e0a0833
EVELO <evelo0702@gmail.com>
2024-09-15 18:14:45 +0900

    feat: notice 리스트 페이지네이션 컴포넌트 생성

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,5 +1,24 @@
-const NoticeListPage = () => {
-  return <div>NoticeListPage</div>;
+import NoticeList from "@/components/notice-list";
+import NoticePagination from "@/components/notice-pagination";
+import { getNoticeData } from "@/lib/getNoticeData";
+import { Notices } from "@/types/type";
+import Link from "next/link";
+
+const NoticeListPage = async () => {
+  let data = await getNoticeData("");
+
+  const sortByDate = (data: Notices[]) => {
+    return data.sort(
+      (b, a) => new Date(a.date).getTime() - new Date(b.date).getTime()
+    );
+  };
+  sortByDate(data);
+
+  return (
+    <div>
+      <NoticePagination data={data} />
+    </div>
+  );
 };
 
 export default NoticeListPage;
---
+++ src/components/notice-pagination.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { Notices } from "@/types/type";
+import Link from "next/link";
+import React, { use, useEffect, useState } from "react";
+
+const NoticePagination = ({ data }: { data: Notices[] }) => {
+  const [pageNum, setPageNum] = useState(0);
+  const [noticeData, setNoticeData] = useState<Notices[]>([]);
+  const itemsPerPage = 10;
+  const totalPage = Math.ceil(data.length / itemsPerPage);
+
+  const sliceData = (pageNum: number) => {
+    setNoticeData(data.slice(pageNum * 10, pageNum * 10 + 10));
+  };
+  useEffect(() => {
+    sliceData(pageNum);
+  }, [pageNum]);
+  return (
+    <div className="relative">
+      <div className="h-[65vh]">
+        {noticeData.map((i) => (
+          <div key={i.id}>
+            <Link href={`/notice/${i.id}`}>
+              <div className="hover:bg-gray-100 border-2 mb-1 p-2 border-neutral-300 rounded-md flex-1 flex items-center justify-between">
+                <div className="ms-4">{i.title}</div>
+                <div>{i.date}</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        {Array.from({ length: totalPage }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => setPageNum(index)}
+            className={`
+                p-2 rounded-md cursor-pointer transition-colors me-4 mt-4 ${
+                  pageNum === index
+                    ? "bg-blue-500 text-white"
+                    : "bg-white text-blue-500 border-blue-500 hover:bg-blue-100"
+                }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NoticePagination;
