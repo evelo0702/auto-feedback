168c2d524f4f985b0decf9c594ffaf7032eb4238
EVELO <evelo0702@gmail.com>
2024-09-20 19:09:34 +0900

    update: api/faqs route post 추가

--- src/app/api/faqs/route.ts
+++ src/app/api/faqs/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/app/utils/database";
-import {  getCategoryName } from "@/app/utils/categoryCode";
+import { getCategoryName } from "@/app/utils/categoryCode";
 
 // GET 요청 처리
 export async function GET() {
@@ -16,6 +16,24 @@ export async function GET() {
     console.error("Error fetching data:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+export async function POST(req: Request) {
+  try {
+    const db = (await connectDB).db("auto-feedback");
+    let collection = db.collection("faqs");
+    const data = await req.json();
+    await collection.insertOne(data);
+    return NextResponse.json(
+      { message: "Data inserted successfully" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.error("Error inserting data:", err);
+    return NextResponse.json(
+      { message: "Error inserting data" },
       { status: 500 }
     );
   }
