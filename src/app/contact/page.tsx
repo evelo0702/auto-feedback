58404b77463167f38468e093cf1421350da63362
EVELO <evelo0702@gmail.com>
2024-09-20 19:14:54 +0900

    feat: 문의 페이지 생성

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,11 @@
-"use client";
+import OpenAi from "@/components/openai";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-full ">
+      <OpenAi />
+    </div>
+  );
 };
 
 export default ContactPage;
