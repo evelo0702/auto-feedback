8479b0aad8e2427c836a4bfa328727ed39357713
EVELO <evelo0702@gmail.com>
2024-09-20 19:14:44 +0900

    feat: 공지사항 페이지 생성

--- src/app/notice/[id]/page.tsx
+++ src/app/notice/[id]/page.tsx
@@ -1,5 +1,24 @@
-const NoticeDetailPage = ({ params }: { params: { id: string } }) => {
-  return <div>NoticeDetailPage {params.id}</div>;
+import { getNoticeData } from "@/lib/getNoticeData";
+
+const NoticeDetailPage = async ({ params }: { params: { id: string } }) => {
+  let data = await getNoticeData("", params.id);
+  return (
+    <div className="px-32 py-10">
+      {data[0] && (
+        <>
+          <div className="flex items-center justify-between text-2xl">
+            {data[0].title} <p className="text-sm">{data[0].date}</p>
+          </div>
+
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data[0].content.replace(/\n/g, "<br />"),
+            }}
+          />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default NoticeDetailPage;
