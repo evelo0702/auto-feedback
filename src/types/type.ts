cc06ccc33231b57ad2ff60abe3ac3d6bcdccd244
EVELO <evelo0702@gmail.com>
2024-09-20 19:11:00 +0900

    update: 타입 추가 및 변경

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,11 +1,40 @@
 export interface Faqs {
+  _id?: any;
   id: number;
   category_code: number;
   content: { question: string; answer: string };
+  category_name?: { eng: string; kor: string };
 }
 export interface Notices {
+  _id?: any;
   id: number;
   title: string;
   content: string;
   category_code: number;
+  category_name?: { eng: string; kor: string };
+  date: string;
+}
+
+export interface Openai {
+  id: number;
+  title?: string;
+  type: string;
+  content?: { question: string; answer: string };
+  newId: number;
+  category_code: number;
+}
+
+export interface Contacts {
+  text: string;
+  category_code: number;
+  date: string;
+  email: string;
+}
+
+export interface Services {
+  _id?: any;
+  category_code: number;
+  text: string;
+  email: string;
+  date: string;
 }
